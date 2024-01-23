3921be5ed24dca5861e703b535e5d760ca982c6c
Cong Huy <conghuy123zzz@gmail.com>
2024-01-23 14:20:21 +0700

    create popper avatar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import useHeader from './hook'
-import { ReceivedProps } from './type'
-import { Avatar } from 'antd'
-import { UserOutlined, FieldTimeOutlined, SearchOutlined, QuestionCircleOutlined } from '@ant-design/icons'
-import './index.scss'
+import useHeader from './hook';
+import { ReceivedProps } from './type';
+import { Avatar } from 'antd';
+import {
+  UserOutlined,
+  FieldTimeOutlined,
+  SearchOutlined,
+  QuestionCircleOutlined,
+} from '@ant-design/icons';
+import './index.scss';
+import { useState } from 'react';
+import Popper from '../Popper/index';
 
 const HeaderLayout = () => {
+  const [popperVisible, setPopperVislible] = useState(false);
+
+  const handleAvatar = () => {
+    setPopperVislible(!popperVisible);
+  };
+
   return (
-    <div className='header'>
+    <div className='header px-4'>
       <div className='header-left'>
-        <Avatar icon={<UserOutlined />} />
         <FieldTimeOutlined className='icon-header-left' />
         {/*Avatar for logged in user */}
       </div>
@@ -16,13 +28,19 @@ const HeaderLayout = () => {
         <SearchOutlined />
         <input placeholder='Search' className='input-search' />
       </div>
-      <div className='header-right'>
+      <div className='header-right relative'>
         <QuestionCircleOutlined className='icon-help' />
+        <Avatar
+          onClick={handleAvatar}
+          icon={<UserOutlined />}
+          className='cursor-pointer'
+        />
+        {!popperVisible && <Popper />}
       </div>
     </div>
-  )
-}
+  );
+};
 const Header = (props: ReceivedProps) => {
-  return <HeaderLayout {...useHeader(props)} />
-}
-export default Header
+  return <HeaderLayout {...useHeader(props)} />;
+};
+export default Header;
